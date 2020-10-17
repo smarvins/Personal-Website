4c4cb4a2508ad546ae4760c717ad5a4191e4155c
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-17 02:24:55 -0400

    deleted background component and shifted classes to home component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Skills from './components/Skills';
 import Services from './components/Services';
 import Contact from './components/Contact';
+import Background from './components/Background';
 import './App.css';
 
 
@@ -11,10 +12,10 @@ function App() {
   return (
     <main>
         <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/skills" component={Skills} />
-          <Route path="/services" component={Services} />
-          <Route path="/contact" component={Contact} />
+            <Route path="/" component={Home} exact />
+            <Route path="/skills" component={Skills} />
+            <Route path="/services" component={Services} />
+            <Route path="/contact" component={Contact} />
         </Switch>
     </main>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ class Home extends React.Component {
   render(){
     const {hour, username} = this.state;
     return (
-      <div>
+      <div class="h-screen bg-red-400 sm:bg-green-400 md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400">
         <h2>{hour < 12 ? `Good Morning!` : `Good evening!`}</h2>
       </div>
     );
