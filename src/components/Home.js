5a11cc95a3bc7127e8370799f5ba9b54a3747814
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-17 01:06:13 -0400

    created a greetings function depending on the time of day

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,31 @@ import React from 'react';
 import '../styles/main.css';
 import '../App.css';
 
-function Home() {
-  return (
-    <div>Home</div>
-  );
-}
+class Home extends React.Component {
+  state = {
+    hour: null,
+  }
 
+  componentDidMount() {
+    this.getHour()
+  }
+
+  getHour = () => {
+   const date = new Date();
+   const hour = date.getHours()
+   this.setState({
+      hour
+   });
+  }
+
+  render(){
+    const {hour, username} = this.state;
+    return (
+      <div>
+        <h2>{hour < 12 ? `Good Morning!` : `Good evening!`}</h2>
+      </div>
+    );
+  }
+
+}
 export default Home;
