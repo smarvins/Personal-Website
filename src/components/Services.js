667476ce61fef5ba6244edba3b402b618e09510b
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-18 02:52:47 -0400

    created template for the service tab

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,7 +4,18 @@ import '../App.css';
 
 function Services() {
   return (
-    <div>Services</div>
+    <div class="flex flex-col font-body h-screen bg-red-400 sm:bg-green-400 md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400">
+      <div>
+        <p class="font-semibold text-xl p-4 text-gray-300">&lt; steve marvins /&gt;</p>
+      </div>
+      <div class="flex-initial">
+
+      </div>
+      <div class="flex-auto bg-white rounded-tl-3xl p-5 ml-2">
+        <p class="text-3xl text-red-400">SERVICES</p>
+        <p class="text-xl">These are the <span class="text-red-400">cool</span> stuff I can do for you.</p>
+      </div>
+    </div>
   );
 }
 
