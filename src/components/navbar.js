ae920b0efb657622677017202c2d31ccff4cf007
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-21 18:47:01 -0400

    spelling checks

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -35,7 +35,7 @@ function Navbar() {
             <svg className="w-12 m-auto bg-gray-100 rounded-full p-3 stroke-current text-red-400 sm:text-green-400 md:text-blue-400 lg:text-pink-400 xl:text-teal-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
             </svg>
-            <p className="font-light text-xl p-2 text-white">work</p>
+            <p className="font-light text-xl p-2 text-white">experience</p>
           </Link>
         </div>
 
