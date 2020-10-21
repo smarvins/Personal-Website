76e938b004625efed7181a6cac947f032213143f
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-21 18:41:49 -0400

    work section is fully responsive

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,7 +3,7 @@ import '../styles/main.css';
 import '../App.css';
 import Navbar from './navbar';
 
-function Work() {
+function Experience() {
   return (
     <div className="flex flex-col font-body h-full bg-red-400 sm:bg-green-400 md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400">
       {/* Navbar */}
@@ -12,77 +12,108 @@ function Work() {
       </div>
       <div className="flex-auto bg-white rounded-tl-3xl p-5 ml-2 overflow-hidden">
         <p className="text-3xl text-red-400 sm:text-green-400 md:text-blue-400 lg:text-pink-400 xl:text-teal-400">WORK</p>
-        <p className="text-xl">These are the <span className="text-red-400 sm:text-green-400 md:text-blue-400 lg:text-pink-400 xl:text-teal-400">skills</span> I learned over the past years as a developer.</p>
+        <p className="text-xl">The <span className="text-red-400 sm:text-green-400 md:text-blue-400 lg:text-pink-400 xl:text-teal-400">projects</span> and <span className="text-red-400 sm:text-green-400 md:text-blue-400 lg:text-pink-400 xl:text-teal-400">articles </span> under my name.</p>
         {/* Card section */}
-        <div className="overflow-y-auto h-full mt-4 grid grid-cols-1 lg:grid-cols-3 gap-1">
+        <div className="overflow-y-auto h-full mt-4 grid grid-cols-1 lg:grid-cols-2 gap-1">
 
-          {/* Web Frameworks card */}
+          {/* Kazi */}
           <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
-              <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Web Frameworks</p>
+              <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Kazi</p>
+              <p className=" text-xl md:text-white">A Django+React hybrid site that can set up a task list for your activities.</p>
+              <p className=" text-xl md:text-white mt-4">Tools used:</p>
               <ul className="list-disc text-xl md:text-white">
-                <li>React</li>
-                <li>Django</li>
+                <li>React </li>
+                <li>Django (DRF)</li>
+                <li>JSX</li>
+                <li>SASS</li>
               </ul>
+              {/* Links */}
+              <div className=" text-xl md:text-white mt-6 text-red-400 sm:text-green-400 md:text-white lg:text-white xl:text-white">
+                <a href="https://kazi-list.netlify.app/" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white">Visit the site</a>
+                <a href="https://github.com/Shavolski/Kazi" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white ml-6">See the code</a>
+              </div>
             </div>
           </div>
 
-          {/* UI Frameworks card */}
+          {/* Web design guidlines */}
           <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
-              <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">UI Frameworks</p>
+              <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Web Design Guidlines</p>
+              <p className=" text-xl md:text-white">This is a minimalistic design site to showcase the guidelines that I follow when designing a project.</p>
+              <p className=" text-xl md:text-white mt-4">Tools used:</p>
               <ul className="list-disc text-xl md:text-white">
-                <li>Bootstrap</li>
-                <li>Tailwindcss (this site uses it!)</li>
-                <li>Material UI / Framer motion</li>
+                <li>Gatsby</li>
+                <li>Bulma</li>
+                <li>SASS</li>
+                <li>GraphQL</li>
               </ul>
+              {/* Links */}
+              <div className=" text-xl md:text-white mt-6 text-red-400 sm:text-green-400 md:text-white lg:text-white xl:text-white">
+                <a href="https://stevemarvins-web-design-guidlines.netlify.app/" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white">Visit the site</a>
+                <a href="https://github.com/Shavolski/Web-Design-Guidelines" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white ml-6">See the code</a>
+              </div>
             </div>
           </div>
 
-          {/* Markup card */}
+          {/* Forex bot */}
           <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
-              <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Markup and Styles</p>
-              <ul className="list-disc text-xl md:text-white">
-                <li>HTML/JSX</li>
-                <li>CSS / SCSS</li>
-                <li>JSS</li>
-              </ul>
-            </div>
-          </div>
-
-          {/* Language card */}
-          <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
-            <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
-              <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Languages</p>
+              <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Forex Bot</p>
+              <p className=" text-xl md:text-white">A bot that uses simple moving averages to trade currencies in the FX market.</p>
+              <p className=" text-xl md:text-white mt-4">Tools used:</p>
               <ul className="list-disc text-xl md:text-white">
                 <li>Python</li>
-                <li>JavaScript</li>
+                <li>OANDA Api</li>
               </ul>
+              {/* Links */}
+              <div className=" text-xl md:text-white mt-6 text-red-400 sm:text-green-400 md:text-white lg:text-white xl:text-white">
+                <a href="https://github.com/Shavolski/Forex-Bot" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white">See the code</a>
+              </div>
             </div>
           </div>
 
-          {/* Hosting card */}
+          {/* Framer motion */}
           <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
-              <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Hosting</p>
+              <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Framer Motion Portfolio</p>
+              <p className=" text-xl md:text-white">This is another portfolio but it is built using Framer Motion...an animation library for React.</p>
+              <p className=" text-xl md:text-white mt-4">Tools used:</p>
               <ul className="list-disc text-xl md:text-white">
-                <li>Github</li>
-                <li>Netlify</li>
-                <li>Heroku</li>
+                <li>React</li>
+                <li>Framer Motion</li>
+                <li>EmailJS</li>
+                <li>React Alert</li>
               </ul>
+              {/* Links */}
+              <div className=" text-xl md:text-white mt-6 text-red-400 sm:text-green-400 md:text-white lg:text-white xl:text-white">
+                <a href="https://steve-marvins.netlify.app/" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white">Visit the site</a>
+                <a href="https://github.com/Shavolski/Framer-Motion-Portfolio" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white ml-6">See the code</a>
+              </div>
             </div>
           </div>
 
-          {/* YouTube card */}
+          {/* Article 1 */}
           <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
-              <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">YouTube</p>
-              <ul className="list-disc text-xl md:text-white">
-                <li>Channel Management</li>
-                <li>Video Editing</li>
-                <li>SEO / Thumbnail Creation</li>
-              </ul>
+              <p className="md:mb-5 w-64 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Object-Oriented Programming…simplified.</p>
+              <p className=" text-xl md:text-white">An article explaining O.O.P as a paradigm and how it functions in the tasks applicable to it.</p>
+              {/* Links */}
+              <div className=" text-xl md:text-white mt-6 text-red-400 sm:text-green-400 md:text-white lg:text-white xl:text-white">
+                <a href="https://medium.com/@stevemarvins.dev/object-oriented-programming-simplified-ddf0e3a628ef" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white">Read more..</a>
+              </div>
+            </div>
+          </div>
+
+          {/* Article 2 */}
+          <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
+            <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
+              <p className="md:mb-5 w-64 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Responsive Grommet-UI grids in React.</p>
+              <p className=" text-xl md:text-white">An article that showcases how to create a responsive grid using Grommet UI in React.</p>
+              {/* Links */}
+              <div className=" text-xl md:text-white mt-6 text-red-400 sm:text-green-400 md:text-white lg:text-white xl:text-white">
+                <a href="https://medium.com/@stevemarvins.dev/responsive-grommet-ui-grids-in-react-31892103c207" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white">Read more..</a>
+              </div>
             </div>
           </div>
 
@@ -94,4 +125,4 @@ function Work() {
   );
 }
 
-export default Work;
+export default Experience;
