7b728db37bf6566a65df5b32fd41832a20954fcd
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-22 21:42:40 -0400

    contact component has responsive animation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,7 @@ import emailjs from 'emailjs-com';
 import '../styles/main.css';
 import '../App.css';
 import Navbar from './navbar';
+import { motion } from "framer-motion";
 
 function Contact() {
   //Email function for contact form
@@ -19,13 +20,35 @@ function Contact() {
       });
   }
 
+  {/* Up Animation */}
+  const container = {
+    hidden: { opacity: 1 },
+    show: {
+      opacity: 1,
+      transition: {
+        type: "spring",
+        stiffness: 150,
+        ease: "easeInOut",
+        staggerChildren: 0.3
+      }
+    }
+  }
+
+  const item = {
+    hidden: { y:-300, opacity: 0 },
+    show: { y:0, opacity: 1 }
+  }
   return (
     <div className="flex flex-col font-body h-full bg-red-400 sm:bg-green-400 md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400">
       {/* Navbar */}
       <div className="flex-initial">
         <Navbar/>
       </div>
-      <div className="flex-auto bg-white rounded-tl-3xl p-5 ml-2 overflow-hidden">
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+        className="flex-auto bg-white rounded-tl-3xl p-5 ml-2 overflow-hidden">
         <p className="text-3xl text-red-400 sm:text-green-400 md:text-blue-400 lg:text-pink-400 xl:text-teal-400">Contact</p>
         <p className="text-xl">Why end the conversation here? Let's have a <span className="text-red-400 sm:text-green-400 md:text-blue-400 lg:text-pink-400 xl:text-teal-400">chat</span>.</p>
         {/* Card section */}
@@ -34,7 +57,9 @@ function Contact() {
           {/* Contact Form */}
           <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md ">
             <div className="md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 sm:p-10 rounded-md md:w-full ">
-              <form className="w-full max-w-sm m-auto" onSubmit={sendEmail}>
+              <motion.form
+                variants={item}
+                className="w-full max-w-sm m-auto" onSubmit={sendEmail}>
 
                 <div className="flex items-center border-b border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white py-2 mb-4">
                   <input className="appearance-none bg-transparent border-none w-full text-red-400 sm:text-green-400 md:text-white mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" name="user_name" aria-label="Full name"/>
@@ -68,7 +93,7 @@ function Contact() {
                   <a href="https://www.linkedin.com/in/stevemarvins-dev/" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white">LinkedIn</a>
                   <a href="https://github.com/Shavolski" target="_blank" rel="noopener noreferrer" className=" border-b-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white ml-6">GitHub</a>
                 </div>
-              </form>
+              </motion.form>
             </div>
           </div>
 
@@ -78,7 +103,7 @@ function Contact() {
         </div>
         <div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
