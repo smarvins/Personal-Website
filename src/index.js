1584ddba2187b2d9418a77e2a42bcdb6d9bee4ef
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-22 20:12:53 -0400

    adjusted email service

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
+
+
 ReactDOM.render(
   <BrowserRouter>
+    <AlertProvider template={AlertTemplate} {...options}>
       <App />
+    </AlertProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
