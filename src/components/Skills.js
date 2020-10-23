5fff511b605c060632aa91afc4cee21d512b54cc
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-22 21:10:51 -0400

    animation test positive

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,6 +5,20 @@ import Navbar from './navbar';
 import { motion } from "framer-motion"
 
 function Skills() {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5
+      }
+    }
+  }
+
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 }
+  }
   return (
     <div className="flex flex-col font-body h-full bg-red-400 sm:bg-green-400 md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400">
       {/* Navbar */}
@@ -15,10 +29,14 @@ function Skills() {
         <p className="text-3xl text-red-400 sm:text-green-400 md:text-blue-400 lg:text-pink-400 xl:text-teal-400">SKILLS</p>
         <p className="text-xl">These are the <span className="text-red-400 sm:text-green-400 md:text-blue-400 lg:text-pink-400 xl:text-teal-400">skills</span> I learned over the past years as a developer.</p>
         {/* Card section */}
-        <div className="overflow-y-auto h-full mt-4 grid grid-cols-1 lg:grid-cols-3 gap-1">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="overflow-y-auto h-full mt-4 grid grid-cols-1 lg:grid-cols-3 gap-1">
 
           {/* Web Frameworks card */}
-          <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
+          <motion.div variants={item} className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
               <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Web Frameworks</p>
               <ul className="list-disc text-xl md:text-white">
@@ -26,10 +44,10 @@ function Skills() {
                 <li>Django</li>
               </ul>
             </div>
-          </div>
+          </motion.div>
 
           {/* UI Frameworks card */}
-          <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
+          <motion.div variants={item} className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
               <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">UI Frameworks</p>
               <ul className="list-disc text-xl md:text-white">
@@ -38,10 +56,10 @@ function Skills() {
                 <li>Material UI / Framer motion</li>
               </ul>
             </div>
-          </div>
+          </motion.div>
 
           {/* Markup card */}
-          <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
+          <motion.div variants={item} className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
               <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Markup and Styles</p>
               <ul className="list-disc text-xl md:text-white">
@@ -50,10 +68,10 @@ function Skills() {
                 <li>JSS</li>
               </ul>
             </div>
-          </div>
+          </motion.div>
 
           {/* Language card */}
-          <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
+          <motion.div variants={item} className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
               <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Languages</p>
               <ul className="list-disc text-xl md:text-white">
@@ -61,10 +79,10 @@ function Skills() {
                 <li>JavaScript</li>
               </ul>
             </div>
-          </div>
+          </motion.div>
 
           {/* Hosting card */}
-          <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
+          <motion.div variants={item} className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
               <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">Hosting</p>
               <ul className="list-disc text-xl md:text-white">
@@ -73,10 +91,10 @@ function Skills() {
                 <li>Heroku</li>
               </ul>
             </div>
-          </div>
+          </motion.div>
 
           {/* YouTube card */}
-          <div className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
+          <motion.div variants={item} className="border-2 border-red-400 sm:border-green-400 md:border-white lg:border-white xl:border-white rounded-md">
             <div className="sm:inline-block md:bg-blue-400 lg:bg-pink-400 xl:bg-teal-400 p-5 pl-10 sm:p-10 rounded-md md:w-full lg:h-full">
               <p className="md:mb-5 w-48 md:text-center text-red-400 sm:text-green-400 md:text-white md:border-2 md:border-white rounded-md text-xl">YouTube</p>
               <ul className="list-disc text-xl md:text-white">
@@ -85,9 +103,9 @@ function Skills() {
                 <li>SEO / Thumbnail Creation</li>
               </ul>
             </div>
-          </div>
+          </motion.div>
 
-        </div>
+        </motion.div>
         <div>
         </div>
       </div>
