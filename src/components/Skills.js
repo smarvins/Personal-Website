1b6d63dac586dd55c1f77d347410a41c71d0793f
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-22 21:07:28 -0400

    installed and imported framer motion

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/main.css';
 import '../App.css';
 import Navbar from './navbar';
+import { motion } from "framer-motion"
 
 function Skills() {
   return (
